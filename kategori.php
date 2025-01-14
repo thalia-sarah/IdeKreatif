e3b58dc6345040200c09a9bb0423a9c70775c5cb
thalia-sarah <ritasaparina1983@gmail.com>
2025-01-14 15:02:23 +0700

    menambah modal update

--- kategori.php
+++ kategori.php
@@ -96,7 +96,32 @@ include '.includes/toast_notification.php';
                             </div>
                         </div>
                         <!-- Modal untuk Update Data Kategori -->
-                        
+                        <div class="modal fade" id="deleteCategory_<?= $category['category_id']; ?>" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Update kategori</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="proses_kategori.php" method="POST">
+                                            <!-- input tersembunyi untuk menyimpan update kategori -->
+                                                <input type="hidden" name="catID" value="<?+ $category['category_id']; ?>">
+                                            <div class="form-group">
+                                                <label>Nama Kategori</label>
+                                                <!-- Input untuk nama kategori -->
+                                                <input type="text" value="<?= $category['category_name']; ?>" name="category_name" class="form-control">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                <button type="submit" name="update" class="btn btn-warning">Update</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <?php endwhile; ?>
                     </tbody>
                 </table>
